d5711761e5b6c1cc3f8c0b5008b92192e21d14c7
Kally <kallyasmedia@gmail.com>
2020-07-22 13:53:15 +0000

    Add event listener to play buttons

--- js/app.js
+++ js/app.js
@@ -6,86 +6,124 @@ let albumCount = document.querySelector(".album-count");
 let name = document.querySelector(".name");
 let tracksContainer = document.querySelector(".tracks-container");
 let songContainer = document.querySelector(".song-container");
+let cover = document.querySelector(".cover img");
 
 window.addEventListener("DOMContentLoaded", () => {
-  let playBtn = [...document.querySelectorAll("button")];
-  playBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      let audio = new Audio(document.querySelector("audio"));
-      if (audio.src != song.previewUrl) {
-        audio.src = song.previewUrl;
-        audio.play();
-        this.textContent("pause");
-
-        if (audio.currentSong) audio.currentSong.textContent("Play");
-        audio.currentSong = this;
-      } else {
-        if (!audio.paused) {
-          this.textContent("play");
-          audio.pause();
-        }
-      }
-    });
+  console.log("content loaded");
+  searchInput.addEventListener("onkeyup", (e) => {
+    query.term = this.value;
+    if (e.key == "Enter") {
+      showAlbums();
+      showTracks();
+    }
   });
-});
 
-searchInput.addEventListener("onkeyup", (e) => {
-  query.term = this.value;
-  if (e.key == "Enter") {
-    showAlbums();
-    showTracks();
+  let query = {
+    term: "ncs",
+    limit: "20",
+    entity: "album",
+  };
+
+  async function getMusic() {
+    let url = new URL(BASE_URL);
+
+    Object.keys(query).forEach((k) => {
+      url.searchParams.append(k, query[k]);
+    });
+    return await (await fetch(url.href)).json();
   }
-});
 
-let query = {
-  term: "ncs",
-  limit: "10",
-  entity: "album",
-};
-
-async function getMusic() {
-  let url = new URL(BASE_URL);
-
-  Object.keys(query).forEach((k) => {
-    url.searchParams.append(k, query[k]);
-  });
-
-  return await (await fetch(url.href)).json();
-}
-
-getMusic().then(console.log);
-showAlbums();
-function showAlbums() {
-  query.entity = "album";
-  getMusic().then((data) => {
-    data.results.forEach((album) => {
-      let img = album.artworkUrl100.replace("100x100", "200x200");
-      albumCount.style.backgroundImage = `url(${img}`;
-      name.textContent = album.collectionName;
+  getMusic().then(console.log);
+  showAlbums();
+  function showAlbums() {
+    query.entity = "album";
+    getMusic().then((data) => {
+      data.results.forEach((album) => {
+        let img = album.artworkUrl100.replace("100x100", "200x200");
+        albumCount.style.backgroundImage = `url(${img}`;
+        name.textContent = album.collectionName;
+        () => {
+          location.hash = album.collectionId;
+        };
+      });
     });
-  });
-}
+  }
 
-showTracks();
+  showTracks();
 
-async function showTracks() {
-  let result = "";
-  query.entity = "song";
+  async function showTracks() {
+    let result = "";
+    query.entity = "song";
 
-  let songs = await getMusic();
+    let songs = await getMusic();
 
-  songs.results.forEach((song) => {
-    songContainer.style.backgroundColor = "#444";
-    result += `
+    songs.results.forEach((song) => {
+      songContainer.style.backgroundColor = "#444";
+      result += `
     <div class="song-container">
       <img src="${song.artworkUrl60}" alt="song cover">
       <div class="info">
       <h1>${song.trackName}</h1>
       <h2>${song.artistName}</h2>
       </div>
-      <button class="play"><i class="fas fa-play"></i><audio src=${song.previewUrl}></audio></button>
+      <button class="play"><i class="fas fa-play"></i></button>
+      <audio src=${song.previewUrl}></audio>
       </div>
       `;
+    });
+    tracksContainer.innerHTML = result;
+  }
+
+  let playBtn = [...document.querySelectorAll("button")];
+  playBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let audio = new Audio(...document.querySelector("audio"));
+      audio.forEach((aud) => {
+        if (aud.src != song.previewUrl) {
+          aud.src = song.previewUrl;
+          aud.play();
+          this.textContent("pause");
+
+          if (aud.currentSong) aud.currentSong.textContent("Play");
+          aud.currentSong = this;
+        } else {
+          if (!aud.paused) {
+            this.textContent("play");
+            aud.pause();
+          }
+        }
+      });
+    });
   });
-  tracksContainer.innerHTML = result;
+});
+
+let album = {};
+
+async function getAlbum(id) {
+  let url = `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${id}&entity=song`;
+  let data = await (await fetch(url)).json();
+  console.log(data);
+  album = data.results;
+  console.log(album);
+  // let img = document.createElement('img')
+  cover.setAttribute(
+    "src",
+    album[0].artworkUrl100.replace("100x100", "512x512")
+  );
+  document.body.appendChild(cover);
+  showPlayer();
+}
+
+onhashchange = () => {
+  if (location.hash) getAlbum(+location.hash.substr(1));
+};
+
+if (onhashchange) {
+  onhashchange();
+}
+
+function showPlayer(d) {
+  var trackNum = 1;
+
+  let percent = 5;
 }
